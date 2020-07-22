a304cd7f476d96f57570b22cdee1475e6e24731c
Ryan Milne <ryanmilne101@gmail.com>
2020-07-22 16:58:09 -0600

    app7 done

---
+++ app-7/src/List.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+import Todo from './Todo'
+
+class List extends Component{
+render(){
+    let list = this.props.tasks.map((el, i)=> {
+        return <Todo key={i} task={el}/>
+    })
+return <div>{list}</div>
+}
+}
+
+export default List
